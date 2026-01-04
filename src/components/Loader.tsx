73df1252a1ff72f3485f6e9f6c2ec8128eddc9f2
SanjuHashintha <sanjuhashintha@gmail.com>
2026-01-04 20:01:32 +0530

    navbar added

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -13,16 +13,15 @@ export default function Loader({ onComplete }: { onComplete: () => void }) {
   }, [onComplete]);
 
   return (
-    <div
-      className="fixed inset-0 z-50 overflow-hidden"
-      style={{ background: "#ffffff" }}
-    >
+    <div className="fixed inset-0 z-50 overflow-hidden">
+      {/* Top panel - pure black */}
       <motion.div
         initial={{ y: 0 }}
         animate={{ y: "-100%" }}
         transition={{ duration: 1.0, ease: "easeInOut" }}
         className="absolute top-0 left-0 w-full h-1/2 bg-black"
       />
+      {/* Bottom panel - pure black */}
       <motion.div
         initial={{ y: 0 }}
         animate={{ y: "100%" }}
